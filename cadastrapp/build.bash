a45a9e9acdece3b73701457e35ffd2b2172ab08f
MaelREBOUX <m.reboux@rennesmetropole.fr>
2021-04-09 18:52:30 +0200

    Update build.bash

--- cadastrapp/build.bash
+++ cadastrapp/build.bash
@@ -1,32 +1,33 @@
 #! /bin/bash
 
-# on supprime tout ce qui est dans _build
-rm -rf html/*
 
-echo "+++++++++++++++++++++++++++++++++++"
+# on supprime tout ce qui est dans _build
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo " purge des builds"
+rm -rf html/*
+echo " fait"
+echo ""
+
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo " liste des versions"
 
 # on va dans le dépôt
 cd repo
 
-# on crée une liste de tags
+# on crée une liste de tags / branches à builder
 liste_versions=()
 
-tags_v1="`git tag -l "*v1.[8-9]*"`"
-for tag in ${tags_v1}; do
-  #echo $tag
-  liste_versions+=($tag)
-done
-
+# une liste de tags
 tags_v2="`git tag -l "*v2.*"`"
 for tag in ${tags_v2}; do
   #echo $tag
   liste_versions+=($tag)
 done
 
-# on rajoute des branches à la main
-liste_versions=("docs_mapstore2")
+# on rajoute des branches à la main + master
 liste_versions+=("master")
+liste_versions+=("docs_mapstore2")
 
 echo " fait"
 echo ""
@@ -35,7 +36,9 @@ echo ""
 # et on itère dessus
 for item in "${liste_versions[@]}"; do
   
-  echo "+++++++++++++++++++++++++++++++++++"
+  echo ""
+  echo ""
+  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
   echo " build de : $item"
 
   # on change de branche
@@ -53,8 +56,14 @@ for item in "${liste_versions[@]}"; do
   git checkout docs/conf.py
   rm -rf docs/_templates
 
+  echo ""
+  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
   echo " patch CSS"
-  sed -i -e 's/max-width:800px/max-width:1200px/g' ../cadastrapp_docs/html/$item/_static/css/theme.css
+  sed -i -e 's/max-width:800px/max-width:1200px/g' ../html/$item/_static/css/theme.css
+  echo " fait"
+  echo ""
+
+  #read -p "Press any key to resume ..."
   
 done
 
