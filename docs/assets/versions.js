7ba194d6f5710898b197c0a3db361331f6e7ae00
Jean-Michel Crepel <jean-michel.crepel@camptocamp.com>
2023-06-01 17:31:10 +0200

    update versions.js

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -31,17 +31,8 @@ window.addEventListener("DOMContentLoaded", function() {
   var rtdData = window['READTHEDOCS_DATA'] || { version: 'latest' };
   var margin = 30;
   var headerHeight = document.getElementsByClassName("md-header")[0].offsetHeight; 
-  if (rtdData.version === latest) {
-    document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for an unreleased version of "+tool_name+", <a href='https://"+documentation_name+".readthedocs.io/en/stable/'>click here to go to the latest stable version.</a></div>"
-    var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
-    document.querySelector("header.md-header").style.top = bannerHeight +"px";   
-    document.querySelector('style').textContent +=
-    "@media screen and (min-width: 76.25em){ .md-sidebar { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
-    document.querySelector('style').textContent +=
-    "@media screen and (min-width: 60em){ .md-sidebar--secondary { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
-  }
-  else if (rtdData.version !== stable_version_name) {
-    document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of "+tool_name+", <a href='https://"+documentation_name+".readthedocs.io/en/stable/'>click here to go to the latest stable version.</a></div>"
+  if (rtdData.version !== latest) {
+    document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for an old version of "+tool_name+", <a href='https://"+documentation_name+".readthedocs.io/en/latest/'>click here to go to the latest stable version.</a></div>"
     var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
     document.querySelector("header.md-header").style.top = bannerHeight +"px";   
     document.querySelector('style').textContent +=
