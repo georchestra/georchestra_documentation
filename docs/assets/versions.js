6f1b5c55719a2dcddd2f521f802d7748a7d989cf
Jean-Michel Crepel <jean-michel.crepel@camptocamp.com>
2023-06-01 17:23:33 +0200

    update versions.js

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -1,3 +1,7 @@
+var tool_name = "Georchestra"
+var documentation_name = 'georchestratdocumentation-test'
+var stable_version_name = "master";
+var latest = "version_test"
 setTimeout(function() {
   const callbackName = 'callback_' + new Date().getTime();
   window[callbackName] = function (response) {
@@ -17,8 +21,8 @@ setTimeout(function() {
   document.getElementsByTagName('head')[0].appendChild(CSSLink);
 
   var script = document.createElement('script');
-  script.src = 'https://georchestratdocumentation-test.readthedocs.io/_/api/v2/footer_html/?'+
-      'callback=' + callbackName + '&project=georchestratdocumentation-test&page=&theme=mkdocs&format=jsonp&docroot=docs&source_suffix=.md&version=' + (window['READTHEDOCS_DATA'] || { version: 'latest' }).version;
+  script.src = 'https://'+documentation_name+'.readthedocs.io/_/api/v2/footer_html/?'+
+      'callback=' + callbackName + '&project='+documentation_name+'&page=&theme=mkdocs&format=jsonp&docroot=docs&source_suffix=.md&version=' + (window['READTHEDOCS_DATA'] || { version: 'latest' }).version;
   document.getElementsByTagName('head')[0].appendChild(script);
 }, 0);
 
@@ -27,8 +31,8 @@ window.addEventListener("DOMContentLoaded", function() {
   var rtdData = window['READTHEDOCS_DATA'] || { version: 'latest' };
   var margin = 30;
   var headerHeight = document.getElementsByClassName("md-header")[0].offsetHeight; 
-  if (rtdData.version === "latest") {
-    document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for an unreleased version of Georchestra, <a href='https://georchestratdocumentation-test.readthedocs.io/en/stable/'>click here to go to the latest stable version.</a></div>"
+  if (rtdData.version === latest) {
+    document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for an unreleased version of "+tool_name+", <a href='https://"+documentation_name+".readthedocs.io/en/stable/'>click here to go to the latest stable version.</a></div>"
     var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
     document.querySelector("header.md-header").style.top = bannerHeight +"px";   
     document.querySelector('style').textContent +=
@@ -36,8 +40,8 @@ window.addEventListener("DOMContentLoaded", function() {
     document.querySelector('style').textContent +=
     "@media screen and (min-width: 60em){ .md-sidebar--secondary { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
   }
-  else if (rtdData.version !== "stable") {
-    document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of Georchestra, <a href='https://georchestratdocumentation-test.readthedocs.io/en/stable/'>click here to go to the latest stable version.</a></div>"
+  else if (rtdData.version !== stable_version_name) {
+    document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of "+tool_name+", <a href='https://"+documentation_name+".readthedocs.io/en/stable/'>click here to go to the latest stable version.</a></div>"
     var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
     document.querySelector("header.md-header").style.top = bannerHeight +"px";   
     document.querySelector('style').textContent +=
